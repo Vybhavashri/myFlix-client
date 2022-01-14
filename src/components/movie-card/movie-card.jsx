814c77f2ce595fe5e8022df832fa1364a36e4314
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-14 01:17:33 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Card, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+    window.open("/", "_self");
+  }
+
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
-      <Card>
-        <Card.Img variant="top" src={movie.Poster} crossOrigin='true' />
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-        </Card.Body>
-      </Card>
+      <Container>
+        <br />
+        <Row>
+          <Card align="center">
+            <Card.Img variant="top" src={movie.Poster} crossOrigin="true" width="300" height="350" />
+            <Card.Body>
+              <Card.Title>{movie.Title}</Card.Title>
+              <Card.Text>{movie.Description}</Card.Text>
+              <br />
+              <Link to={`/movies/${movie._id}`}>
+                <Button variant="link">Show more</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     );
   }
 }
