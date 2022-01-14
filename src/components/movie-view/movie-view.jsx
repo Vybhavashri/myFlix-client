da6e8074d7a2db4a1c926dab39121a56fac00a7a
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-14 01:17:29 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
-let imgStyle = {
-  height: '500px'
-}
+import PropTypes from "prop-types";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class MovieView extends React.Component {
 
@@ -20,47 +17,97 @@ class MovieView extends React.Component {
     document.removeEventListener('keypress', this.keypressCallback);
   }
 
+  addToFavs() {
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    const { movie } = this.props;
+
+    axios.post(`https://myflix-moviedatabaseapp.herokuapp.com/users/${Username}/movies/${movie._id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => {
+        console.log(response);
+        console.log(movie._id);
+        alert("The movie is now on your list.");
+        this.componentDidMount();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+    window.open("/", "_self");
+  }
+
   render() {
-    const { movie, onBackClick } = this.props;
+    const { movie, onBackClick, user } = this.props;
 
     return (
-      <Form>
-        <div className="movie-view">
-          <div className="movie-poster">
-            <img src={movie.Poster} style={imgStyle} crossOrigin='true' />
-          </div>
-          <div className="movie-title">
-            <span className="label">Title: </span>
-            <span className="value">{movie.Title}</span>
-          </div>
-          <div className="movie-description">
-            <span className="label">Description: </span>
-            <span className="value">{movie.Description}</span>
-          </div>
-          <div className="movie-genre">
-            <span className="label">Genre: </span>
-            <span className="value">{movie.Genre.Name}-{movie.Genre.Description}</span>
-          </div>
-          <div className="movie-director">
-            <span className="label">Director: </span>
-            <span className="value">{movie.Director.Name}-{movie.Director.Bio}-{movie.Director.Birth}</span>
-          </div>
-          <div className="movie-featured">
-            <span className="label">Featured: </span>
-            <span className="value">{movie.Featured}</span>
-          </div>
-          <div className="movie-releaseyear">
-            <span className="label">Release Year: </span>
-            <span className="value">{movie.ReleaseYear}</span>
-          </div>
-          <div className="movie-actors">
-            <span className="label">Actors: </span>
-            <span className="value">{movie.Actors}</span>
-          </div>
-          <Button onClick={() => { onBackClick(null); }}>Back</Button>
-        </div>
-      </Form>
+      <Container fluid className="moviesContainer" align="center">
+        <Row>
+          <Col>
+            <br />
+            <div className="movie-view">
+              <div className="movie-poster" style={{ textAlign: "center", marginBottom: "30px" }}>
+                <img src={movie.Poster} crossOrigin="true" width="300" height="400" />
+              </div>
+              <div className="movie-title">
+                <span className="title">Title: </span>
+                <span className="value">{movie.Title}</span>
+              </div>
+              <div className="movie-description">
+                <span className="description">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </div>
+              <div className="movie-genre">
+                <span className="label">Genre: </span>
+                <Link to={`/genres/${movie.Genre.Name}`}>
+                  <span className="value">{movie.Genre.Name}</span>
+                </Link>
+              </div>
+              <div className="movie-director">
+                <span className="director">Director: </span>
+                <Link to={`/directors/${movie.Director.Name}`}>
+                  <span className="value">{movie.Director.Name}</span>
+                </Link>
+              </div>
+              <br />
+              <div className="movie-button-div">
+                <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+                <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { this.addToFavs(); }}>Add to Favorite</Button>
+              </div>
+              <br />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Featured: PropTypes.bool,
+    ReleaseYear: PropTypes.number,
+    Actors: PropTypes.array,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired
+    }),
+    Poster: PropTypes.string.isRequired
+  }).isRequired,
+};
+
 export default MovieView;
