c7226c1ad21c257652ecebcf13f6c60a2016050e
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-19 04:57:41 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -92,23 +92,4 @@ class MovieView extends React.Component {
   }
 }
 
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
-    Featured: PropTypes.bool,
-    ReleaseYear: PropTypes.number,
-    Actors: PropTypes.array,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired
-    }),
-    Poster: PropTypes.string.isRequired
-  }).isRequired,
-};
-
 export default MovieView;
