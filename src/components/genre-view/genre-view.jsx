ce468169bddb91b5830c0c46088cbb9ec14395e7
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-21 06:27:39 +0100

    Update genre-view.jsx

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,33 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Col, Row, Button } from "react-bootstrap";
+import axios from "axios";
+import { Container, Card, Row, Button } from "react-bootstrap";
 
 import { Link } from 'react-router-dom';
 
 import "./genre-view.scss";
 
 function GenreView(props) {
-  const { Genre } = props;
-
-  console.log(Genre, 'genre')
+  const { Genre, onBackClick } = props;
 
   return (
-    <Row className="genre-view">
-      <Col>
-        <div className="genre-name">
-          <span className="label">Name: </span>
-          <span className="value">{Genre.Name}</span>
-        </div>
-        <div className="genre-description">
-          <span className="label">Description: </span>
-          <span className="value">{Genre.Description}</span>
-        </div>
-        <div className="backButton">
-          <Button size="md" variant="outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
-        </div>
-      </Col>
-    </Row>
+    <Container>
+      <br /><br /><br /><br /><br />
+      <Row className="genre-view">
+        <Card align="center">
+          <Card.Body>
+            <Card.Title>{Genre.Name}</Card.Title>
+            <Card.Text>{Genre.Description}</Card.Text>
+            <br />
+            <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
   )
 }
+
+GenreView.propTypes = {
+  Genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default GenreView;
