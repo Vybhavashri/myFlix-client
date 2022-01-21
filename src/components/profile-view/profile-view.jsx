61a6bccc15e7f889b8a1df0e86da9208c15e3d95
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-21 06:29:24 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -34,6 +34,10 @@ class ProfileView extends React.Component {
     window.open("/", "_self");
   }
 
+  onBackClick() {
+    history.goBack()
+  }
+
   getUser = (token) => {
     const Username = localStorage.getItem("user");
     axios
@@ -96,7 +100,7 @@ class ProfileView extends React.Component {
     const Username = localStorage.getItem("user");
     const token = localStorage.getItem("token");
 
-    axios.delete(`https://myflix-moviedatabaseapp.herokuapp.com/users/${Username}`, {
+    axios.delete(`https://myflix-moviedatabaseapp.herokuapp.com/users/delete/${Username}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => {
@@ -145,102 +149,62 @@ class ProfileView extends React.Component {
 
     return (
       <Container className="profile-view" align="center">
-        <Row>
-          <Col>
-            <Card className="user-profile">
-              <Card.Title>User Profile</Card.Title>
-              <Card.Text>
-                <div className="profile-container">
-                  <span className="label">Username: </span>
-                  <span className="value">{Username}</span>
-                  <br />
-                  <br />
-                  <span className="label">EmailID: </span>
-                  <span className="value">{EmailID}</span>
-                  <br />
-                  <br />
-                  <span className="label">Birth: </span>
-                  <span className="value">{Birth}</span>
-                </div>
-              </Card.Text>
-            </Card>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <Card className="update-profile">
-              <Card.Body>
-                <Card.Title>Update Profile</Card.Title>
-                <Form className="update-form" onSubmit={(e) =>
-                  this.editUser(e, this.Username, this.Password, this.EmailID, this.Birth)
-                }>
-                  <Form.Group>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" name="Username" placeholder="New Username" onChange={(e) => this.setUsername(e.target.value)} required />
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="Password" placeholder="New Password" onChange={(e) => this.setPassword(e.target.value)} required />
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Form.Label>EmailID</Form.Label>
-                    <Form.Control type="email" name="Email" placeholder="Enter EmailID" onChange={(e) => this.setEmailID(e.target.value)} required />
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Form.Label>Birth</Form.Label>
-                    <Form.Control type="date" name="Birth" onChange={(e) => this.setBirth(e.target.value)} />
-                  </Form.Group>
-                  <br />
-                  <div className="bt">
-                    <Button variant="warning" type="submit" onClick={this.editUser}>Update User</Button>
-                    <Button className="delete-button" variant="danger" onClick={() => this.onDeleteUser()} > Delete User </Button>
-                  </div>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <br />
-        <Card>
-          <Row style={{ marginTop: "20px" }}>
-            <Col>
-              <h4>{Username} Favorite Movies</h4>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Card.Body>
-                {FavoriteMovies.length === 0 && (
-                  <div className="text-center">No Favorite Movie</div>
-                )}
-                <Row className="favorite-container">
-                  {FavoriteMovies.length > 0 && movies.map((movie) => {
-                    if (movie._id === FavoriteMovies.find((fav) => fav === movie._id)) {
-                      return (
-                        <Card className="favorite-movie card-content" key={movie._id} >
-                          <Card.Img className="fav-poster" variant="top" src={movie.Poster} crossOrigin="true" />
-                          <Card.Body style={{ backgroundColor: "black" }}>
-                            <Card.Title className="movie_title">
-                              {movie.Title}
-                            </Card.Title>
-                            <Button size="sm" variant="danger" value={movie._id} onClick={(e) => this.onRemoveFavorite(e, movie)} > Remove </Button>
-                          </Card.Body>
-                        </Card>
-                      );
-                    }
-                  })}
-                </Row>
-              </Card.Body>
-            </Col>
-          </Row>
+        <br /><br /><br /><br /><br />
+        <Card className="user-profile">
+          <Card.Title>User Profile</Card.Title>
+          <Card.Text>
+            <div className="profile-container">
+              <span className="label">Username: </span>
+              <span className="value">{Username}</span>
+              <br />
+              <br />
+              <span className="label">EmailID: </span>
+              <span className="value">{EmailID}</span>
+              <br />
+              <br />
+              <span className="label">Birth: </span>
+              <span className="value">{Birth}</span>
+            </div>
+          </Card.Text>
         </Card>
+
+        <br />
+
+        <Card className="update-profile">
+          <Card.Body>
+            <Card.Title>Update Profile</Card.Title>
+            <Form className="update-form" onSubmit={(e) => this.editUser(e, this.Username, this.Password, this.EmailID, this.Birth)}>
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" name="Username" placeholder="New Username" onChange={(e) => this.setUsername(e.target.value)} required />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="Password" placeholder="New Password" onChange={(e) => this.setPassword(e.target.value)} required />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>EmailID</Form.Label>
+                <Form.Control type="email" name="Email" placeholder="Enter EmailID" onChange={(e) => this.setEmailID(e.target.value)} required />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Birth</Form.Label>
+                <Form.Control type="date" name="Birth" onChange={(e) => this.setBirth(e.target.value)} />
+              </Form.Group>
+              <br />
+              <Button variant="outline-primary" className="btn-outline-primary" type="submit" onClick={this.editUser}>Update User</Button>
+              <Button variant="outline-danger" onClick={() => this.onDeleteUser()} > Delete User </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+
         <br />
         <div className="backButton">
-          <Button size="md" variant="outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+          <Link to={`/`}>
+            <Button variant="outline-primary">Home</Button>
+          </Link>
         </div>
         <br />
       </Container>
