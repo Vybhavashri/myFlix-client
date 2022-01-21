3321baa652ce06554a02db7be7584278438bd7e9
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-21 06:29:06 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import axios from 'axios';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class MovieView extends React.Component {
-
-  keypressCallback(event) {
-    console.log(event.key);
-  }
-
-  componentDidMount() {
-    document.addEventListener('keypress', this.keypressCallback);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keypress', this.keypressCallback);
-  }
 
   addToFavs() {
     const Username = localStorage.getItem("user");
@@ -30,7 +18,24 @@ class MovieView extends React.Component {
         console.log(response);
         console.log(movie._id);
         alert("The movie is now on your list.");
-        this.componentDidMount();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  removeFromFavs() {
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    const { movie } = this.props;
+
+    axios.delete(`https://myflix-moviedatabaseapp.herokuapp.com/users/${Username}/delete/${movie._id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => {
+        console.log(response);
+        console.log(movie._id);
+        alert("The movie is now deleted from your list.");
       })
       .catch(function (error) {
         console.log(error);
@@ -50,46 +55,54 @@ class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Container fluid className="moviesContainer" align="center">
+      < Container>
+        <br /><br /><br /><br /><br />
         <Row>
-          <Col>
-            <br />
-            <div className="movie-view">
-              <div className="movie-poster" style={{ textAlign: "center", marginBottom: "30px" }}>
-                <img src={movie.Poster} crossOrigin="true" width="300" height="400" />
-              </div>
-              <div className="movie-title">
-                <span className="title">Title: </span>
-                <span className="value">{movie.Title}</span>
-              </div>
-              <div className="movie-description">
-                <span className="description">Description: </span>
-                <span className="value">{movie.Description}</span>
-              </div>
-              <div className="movie-genre">
-                <span className="label">Genre: </span>
-                <Link to={`/movies/genre/${movie.Genre.Name}`}>
-                  <span className="value">{movie.Genre.Name}</span>
-                </Link>
-              </div>
-              <div className="movie-director">
-                <span className="director">Director: </span>
-                <Link to={`/movies/directors/${movie.Director.Name}`}>
-                  <span className="value">{movie.Director.Name}</span>
-                </Link>
-              </div>
+          <Card align="center">
+            <Card.Img variant="top" src={movie.Poster} crossOrigin="true" width="250" height="350" />
+            <Card.Body>
+              <Card.Title>{movie.Title}</Card.Title>
+              <Card.Text>{movie.Description}</Card.Text>
+              <label>Genre:</label>
+              <Link to={`/movies/genre/${movie.Genre.Name}`}>
+                <span className="value">{movie.Genre.Name}</span>
+              </Link>
               <br />
-              <div className="movie-button-div">
-                <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
-                <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { this.addToFavs(); }}>Add to Favorite</Button>
-              </div>
+              <label>Director:</label>
+              <Link to={`/movies/directors/${movie.Director.Name}`}>
+                <span className="value">{movie.Director.Name}</span>
+              </Link>
               <br />
-            </div>
-          </Col>
+            </Card.Body>
+          </Card>
+          <div align="center">
+            <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+            <Button variant="outline-success" className="btn-outline-primary" onClick={() => { this.addToFavs(); }}>Add to Favorite</Button>
+            <Button variant="outline-danger" className="btn-outline-primary" onClick={() => { this.removeFromFavs(); }}>Remove from Favorite</Button>
+          </div>
         </Row>
-      </Container>
+      </Container >
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Featured: PropTypes.bool,
+    ReleaseYear: PropTypes.number,
+    Actors: PropTypes.array,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired
+    }),
+    Poster: PropTypes.string.isRequired
+  })
+};
 
 export default MovieView;
