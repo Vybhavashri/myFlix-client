d8189139209eca56c9e20a36627757dceec4e98b
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-03-01 04:01:25 +0100

    Update navbar-view.jsx

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Navbar, Container, Nav, Button, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import "./navbar-view.scss";
-
 function NavBarView() {
   const user = localStorage.getItem("user");
 
@@ -12,22 +10,28 @@ function NavBarView() {
     window.open("/", "_self");
   };
 
+  home = () => {
+    window.open("/", "_self")
+  }
+
   return (
-    <Navbar className="navbar" variant="dark" expand="lg md" fixed="top" align="center">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="me-auto navbar-menu">
-            <NavItem>
-              Logged in as: <Link to={`/users/${user}`} >{user}</Link>
-            </NavItem>
-          </Nav>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand>
+            <Nav.Link className="d-flex">
+              <Button variant="light" className="button" onClick={() => { this.home() }}>MyFlix</Button>
+            </Nav.Link>
+          </Navbar.Brand>
+          <NavItem className="text-white">
+            Edit Profile : <Link size="lg" to={`/users/${user}`}>{user}</Link>
+          </NavItem>
           <Nav.Link className="d-flex">
-            <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { this.onLoggedOut() }}>Logout</Button>
+            <Button variant="primary" className="btn-primary" onClick={() => { this.onLoggedOut() }}>Logout</Button>
           </Nav.Link>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
